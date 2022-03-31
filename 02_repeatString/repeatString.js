f82c9fd94eb4a74cb6812db63c8e5aeb3ef7f932
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-03-30 19:54:29 -0600

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(input, num) {
+    if(num < 0){
+        return "ERROR"
+    }
+    let string = ""
+    for (let i = 0; i < num; i++){
+        string += input
+    }
+    return string
 };
 
 // Do not edit below this line
