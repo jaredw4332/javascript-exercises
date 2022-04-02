e4a04fd8bb764ea96957af58ddc5b14ed2d0d307
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-01 21:38:08 -0600

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...filter) {
+    const filterArray = []
+    filter.forEach(item => filterArray.push(item));
+    const newArray = array.filter(element => !filterArray.includes(element))
+    return newArray
 };
 
 // Do not edit below this line
