fa2f7da5e9178d4808d74886a85fdb72f1d75a31
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-02 17:50:17 -0600

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+if(intOne < 0 || intTwo < 0 || typeof(intOne) != 'number' || typeof(intTwo) != 'number'){
+    return "ERROR"
+}
+
+if(intOne > intTwo){
+    [intOne, intTwo] = [intTwo, intOne]
+}
+
+
+let rangeInt = intOne
+
+for (rangeInt; rangeInt < intTwo + 1; ++rangeInt){
+    sum = intOne += rangeInt;
+    sum -= 1
+    }
+    return sum
 
 };
 
