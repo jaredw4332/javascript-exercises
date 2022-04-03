585af699750add81db7349aec1743a42bffe0d0b
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-02 18:48:21 -0600

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * 0.55555555555
+  return Math.round(temp * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * 1.8 + 32
+  return Math.round(temp * 10) / 10
 };
 
 // Do not edit below this line
