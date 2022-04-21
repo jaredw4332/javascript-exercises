01c98ce0d3fe0f56c65ece9fad30e1ef4a979327
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-20 20:18:56 -0600

    Update calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a - b
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let sum = 0
+  for (let i = 0; i < array.length; i++){
+    sum += array[i]
+  }
+  return sum
 	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiple = 1
+  for (let i = 0; i< array.length; i++){
+    multiple *= array[i]
+  }
+  return multiple
 };
 
-const power = function() {
-	
+const power = function(number, powerOf) {
+  let finalNumber = number
+  for (let i = 0; i < powerOf - 1; i++){
+    finalNumber *= number
+  }
+	return finalNumber
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0){
+    return 1
+  }
+  for (let i = num - 1; i != 0; i--){
+    num *= i
+  }
+  return num
 };
 
 // Do not edit below this line
