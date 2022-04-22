d8b2954d7da4337187b0df8c72e18a88a66cd932
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-21 21:16:51 -0600

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase()
+    let palindrome = string.replace(/[^0-9a-z]/gi, "")
+    let palindromeArray = palindrome.split('')
+    let palindromeReverse = palindromeArray.reverse()
+    let palindromeResult = palindromeReverse.join('')
+    return palindrome == palindromeResult
 };
 
 // Do not edit below this line
