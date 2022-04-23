2da45a997ffbdb5212fd0814df8ab65d326f850e
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-22 18:33:21 -0600

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
-};
+const getTheTitles = function(books) {
+return books.map(a => a.title)
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
