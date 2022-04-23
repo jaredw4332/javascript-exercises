4c874689739f9074ec821e1b8eb2909e6cb3f2e1
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-22 18:11:16 -0600

    Update fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+let fibonacci = function(int) {
+    if( int < 0){
+        return "OOPS"
+    }
+    let fibonacciArray = [1, 1]
+    for (let i = 0; i < int; i++) { 
+        let fibonacciMinusOne = fibonacciArray[fibonacciArray.length - 1]
+        let fibonacciMinusTwo = fibonacciArray[fibonacciArray.length - 2]
+        fibonacciArray.push(fibonacciMinusOne + fibonacciMinusTwo)
+    }
+    return fibonacciArray[int-1]
 };
 
 // Do not edit below this line
