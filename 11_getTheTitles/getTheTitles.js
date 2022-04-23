49f7ff8e747cee72a862b9fbcabc9417519393f8
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-22 18:37:31 -0600

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,14 +1,3 @@
-const books = [
-    {
-      title: 'Book',
-      author: 'Name'
-    },
-    {
-      title: 'Book2',
-      author: 'Name2'
-    }
-  ]
-
 const getTheTitles = function(books) {
 return books.map(a => a.title)
 }
