0b85c315450424737b1dae7fff4042f50f9a2701
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2022-04-23 20:48:48 -0600

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
 const findTheOldest = function() {
+    let currentYear = new Date().getFullYear()
+    const oldestPerson = people.sort(function(a,b){
+        if (a.yearOfDeath == undefined){
+            a.yearOfDeath = currentYear
+        }
+        if (b.yearOfDeath == undefined){
+            b.yearOfDeath = currentYear
 
+        }
+        const lastPerson = a.yearOfDeath - a.yearOfBirth
+        const nextPerson = b.yearOfDeath - b.yearOfBirth
+        return lastPerson > nextPerson ? -1 : 1
+    })
+    return oldestPerson[0]
 };
 
 // Do not edit below this line
